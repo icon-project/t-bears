8dca07e0e9908f7aeccdf092aaa7a056dfc2c792
Yongwoo Lee <whylee259@gmail.com>
2018-05-09 12:25:00 +0900

    Setup environment for packaging that makes the project executable (#1)

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+requires = [
+    'iconservice',
+    # 'Flask'
+]
+
+
+setup_options = {
+    'name': 'tbears',
+    'version': '0.0.1',
+    'description': '`tbears` for ICON SCORE development',
+    'author': 'ICON foundation',
+    'packages': find_packages(exclude=['tests*','docs']),
+    'py_modules': ['tbears_cli', 'tbears_exception'],
+    'license': "Apache License 2.0",
+    'install_requires': requires,
+    'entry_points': {
+        'console_scripts': [
+            'tbears=tbears_cli:main'
+        ],
+    },
+    'classifiers': [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers', 
+        'Intended Audience :: System Administrators',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6'
+    ]
+}
+
+setup(**setup_options)
