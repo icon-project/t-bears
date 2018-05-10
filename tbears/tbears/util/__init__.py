05bc05e56dec923d47ae5c7c36dbbad155ca5c8e
inwonKim <inwonkim@icon.foundation>
2018-05-10 12:24:21 +0900

    Updated score template.

--- tbears/tbears/util/__init__.py
+++ tbears/tbears/util/__init__.py
@@ -41,19 +41,14 @@ def get_score_main_template(score_class: str) -> str:
     """
     template = """from iconservice import *
 
-    ############################################# 
-    #                                           #
-    #  Refer this contents to write 'score'.    #
-    #                                           #
-    #############################################
 @score
-class token(IconScoreBase):
+class SampleToken(IconScoreBase):
 
     _BALANCES = 'balances'
     _TOTAL_SUPPLY = 'total_supply'
 
-    def __init__(self, db: IconScoreDatabase, owner: Address) -> None:
-        super().__init__(db, owner)
+    def __init__(self, db: IconScoreDatabase, addr_owner: Address) -> None:
+        super().__init__(db, addr_owner)
         self._total_supply = VarDB(self._TOTAL_SUPPLY, db, value_type=int)
         self._balances = DictDB(self._BALANCES, db, value_type=int)
 
@@ -69,27 +64,24 @@ class token(IconScoreBase):
 
     @external(readonly=True)
     def total_supply(self) -> int:
-        print(self._total_supply.get())
         return self._total_supply.get()
 
     @external(readonly=True)
     def balance_of(self, addr_from: Address) -> int:
-        var = self._balances[addr_from]
-        if var is None:
-            var = 0
-        return var
+        return self._balances[addr_from]
 
     def _transfer(self, _addr_from: Address, _addr_to: Address, _value: int) -> bool:
 
         if self.balance_of(_addr_from) < _value:
             raise IconScoreBaseException(f"{_addr_from}'s balance < {_value}")
-        self._balances[_addr_from] = self.balance_of(_addr_from) - _value
-        self._balances[_addr_to] = _value
+
+        self._balances[_addr_from] = self._balances[_addr_from] - _value
+        self._balances[_addr_to] = self._balances[_addr_from] + _value
         return True
 
     @external()
     def transfer(self, addr_to: Address, value: int) -> bool:
-        return self._transfer(self.address, addr_to, value)
+        return self._transfer(self.msg.sender, addr_to, value)
 
     def fallback(self) -> None:
         pass
