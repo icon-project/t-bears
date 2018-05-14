c2a3f495d24600571fa4e4d5cbec0048f06ffdce
inwonKim <inwonkim@icon.foundation>
2018-05-14 10:27:19 +0900

    Deleted unused unit test.

--- tests/test_tbears.py
+++ tests/test_tbears.py
@@ -14,10 +14,8 @@
 # limitations under the License.
 
 import io
-import shutil
 import unittest
-
-from tbears import *
+from tbears.tbears.command import *
 
 DIRECTORY_PATH = os.path.abspath((os.path.dirname(__file__)))
 
@@ -25,39 +23,6 @@ DIRECTORY_PATH = os.path.abspath((os.path.dirname(__file__)))
 class TestTBears(unittest.TestCase):
     def setUp(self):
         self.path = './'
-        self.compress_test_path = os.path.join(DIRECTORY_PATH, '../')
-
-    @staticmethod
-    def touch(path):
-        with open(path, 'a'):
-            os.utime(path, None)
-
-    @staticmethod
-    def extract_files_gen(data: 'bytes'):
-        with zipfile.ZipFile(io.BytesIO(data)) as memory_zip:
-            for zip_info in memory_zip.infolist():
-                with memory_zip.open(zip_info) as file:
-                    file_path = zip_info.filename
-                    root_directory_index = file_path.find("/")
-                    path_uncovered_root_directory = file_path[root_directory_index + 1:]
-                    file_name_start_index = path_uncovered_root_directory.rfind('/')
-                    parent_directory = path_uncovered_root_directory[:file_name_start_index]
-
-                    if path_uncovered_root_directory.find('__MACOSX') != -1:
-                        continue
-                    if path_uncovered_root_directory.find('__pycache__') != -1:
-                        continue
-                    if file_name_start_index == len(path_uncovered_root_directory) - 1:
-                        # continue when 'file_path' is a directory.
-                        continue
-                    if path_uncovered_root_directory.find('/.') != -1:
-                        # continue when 'file_path' is hidden directory or hidden file.
-                        continue
-                    parent_directory = path_uncovered_root_directory[:file_name_start_index]
-                    if file_name_start_index == -1:
-                        yield path_uncovered_root_directory, file, ''
-                    else:
-                        yield path_uncovered_root_directory, file, parent_directory
 
     @staticmethod
     def read_zipfile_as_byte(archive_path: 'str') -> 'bytes':
@@ -87,30 +52,6 @@ class TestTBears(unittest.TestCase):
         main = package_json['main_file']
         self.assertEqual(project_name, main)
         shutil.rmtree(project_name)
-
-    def test_compress(self):
-        project_name = 'tbear'
-        ret1 = compress(project_name, self.compress_test_path)
-        self.assertEqual(ExitCode.SUCCEEDED.value, ret1)
-        self.assertTrue(os.path.isfile(f'./{project_name}.zip'))
-
-        zip_file_info_gen = self.extract_files_gen(self.read_zipfile_as_byte(f'./{project_name}.zip'))
-        compressed_list = [name for name, info, parent_dir in zip_file_info_gen]
-
-        compress_contents = []
-        for directory, dirs, filename in os.walk('./sampleTbears'):
-            parent_directory_index = directory.rfind('/')
-            parent_dir_name = directory[parent_directory_index + 1:]
-            for file in filename:
-                if parent_dir_name.find("__pycache__") != -1:
-                    continue
-                if parent_dir_name == 'sampleTbears':
-                    compress_contents.append(file)
-                else:
-                    compress_contents.append(f'{parent_dir_name}/{file}')
-
-        self.assertTrue(compress_contents.sort() == compressed_list.sort())
-        os.remove('tbear.zip')
 
 
 if __name__ == "__main__":
