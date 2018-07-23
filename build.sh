ea1c24b386b8aed2a8e254d33b0db863c016bcbb
leeheonseung <mycom22@icon.Foundation>
2018-07-23 11:02:29 +0900

    fix build.sh (#73)

--- build.sh
+++ build.sh
@@ -11,23 +11,24 @@ if [[ ("$1" = "test" && "$2" != "--ignore-test") || ("$1" = "build") || ("$1" = 
   pip3 install -r requirements.txt
 
   VER=$(cat VERSION)
-  mkdir -p $VER
+  mkdir -p ${VER}
 
   WGET_VER=$(curl http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/earlgrey/VERSION)
-  pip install --force-reinstall "http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/earlgrey/earlgrey-${WGET_VER}-py3-none-any.whl" -P $VER
+  pip install --force-reinstall "http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/earlgrey/earlgrey-${WGET_VER}-py3-none-any.whl"
   rm -rf earlgrey*
   WGET_VER=$(curl http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/iconcommons/VERSION)
-  pip install --force-reinstall "http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/iconcommons/iconcommons-${WGET_VER}-py3-none-any.whl" -P $VER
+  pip install --force-reinstall "http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/iconcommons/iconcommons-${WGET_VER}-py3-none-any.whl"
   rm -rf iconsommons*
 
   if [[ -z "${ICONSERVICEPATH}" || ("$1" = "deploy") ]]; then
-    wget "http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/$VER/iconservice-$VER-py3-none-any.whl" -P $VER
-    pip install --force-reinstall $VER/iconservice-$VER-py3-none-any.whl
+    WGET_VER=$(curl http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/iconservice/VERSION)
+    pip install --force-reinstall "http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/iconservice/iconservice-${WGET_VER}-py3-none-any.whl"
+    rm -rf iconservice*
   else
     if [ "$(pip3 list | grep iconservice)" ]; then
         pip uninstall iconservice -y
     fi
-    export PYTHONPATH=$ICONSERVICEPATH:$PYTHONPATH
+    export PYTHONPATH=${ICONSERVICEPATH}:${PYTHONPATH}
   fi
 
   if [[ "$2" != "--ignore-test" ]]; then
@@ -41,12 +42,12 @@ if [[ ("$1" = "test" && "$2" != "--ignore-test") || ("$1" = "build") || ("$1" = 
     python setup.py bdist_wheel
 
     if [ "$1" = "deploy" ]; then
-      cp dist/*$VER*.whl $VER
-      cp docs/tbears_jsonrpc_api_v3.md docs/tbears_tutorial.md $VER
-      wget "http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/$VER/CHANGELOG.md" -P $VER
-      wget "http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/$VER/dapp_guide.md" -P $VER
-      tar -cvzf tbears-$VER.tar.gz $VER/*.whl $VER/*.md
-      mv tbears-$VER.tar.gz $VER
+      cp dist/*${VER}*.whl ${VER}
+      cp docs/tbears_jsonrpc_api_v3.md docs/tbears_tutorial.md ${VER}
+      wget "http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/${VER}/CHANGELOG.md" -P ${VER}
+      wget "http://tbears.icon.foundation.s3-website.ap-northeast-2.amazonaws.com/${VER}/dapp_guide.md" -P ${VER}
+      tar -cvzf tbears-$VER.tar.gz ${VER}/*.whl ${VER}/*.md
+      mv tbears-${VER}.tar.gz ${VER}
 
       if [[ -z "${AWS_ACCESS_KEY_ID}" || -z "${AWS_SECRET_ACCESS_KEY}" ]]; then
         echo "Error: AWS keys should be in your environment"
@@ -54,12 +55,12 @@ if [[ ("$1" = "test" && "$2" != "--ignore-test") || ("$1" = "build") || ("$1" = 
       fi
 
       pip install awscli
-      aws s3 cp $VER/tbears-$VER.tar.gz s3://tbears.icon.foundation --acl public-read
-      aws s3 cp $VER/CHANGELOG.md s3://tbears.icon.foundation --acl public-read
+      aws s3 cp ${VER}/tbears-${VER}.tar.gz s3://tbears.icon.foundation --acl public-read
+      aws s3 cp ${VER}/CHANGELOG.md s3://tbears.icon.foundation --acl public-read
     fi
   fi
 
-  rm -rf $VER
+  rm -rf ${VER}
 else
   echo "Usage: build.sh [test|build|deploy]"
   echo "  test: run test"
