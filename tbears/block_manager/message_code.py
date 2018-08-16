89d90e91f8670ac4d6ea20c52ab94056fa8e403d
eunsoopark <eunsoo.park@icon.foundation>
2018-08-16 21:46:46 +0900

    Update the copyright headers (#98)

--- tbears/block_manager/message_code.py
+++ tbears/block_manager/message_code.py
@@ -1,4 +1,4 @@
-# Copyright 2017 theloop Inc.
+# Copyright 2017 ICON Foundation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
