df1ad5d03175f1c5862de902a942247070f69108
spl3en <spl3en.contact@gmail.com>
2018-08-30 21:25:51 +0200

    T-Bears does not use jsonrpcserver package since version 1.0.3, remove it entirely from dependencies.

--- tests/test_util/jsonschema_validator.py
+++ tests/test_util/jsonschema_validator.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from jsonrpcserver import status
 from jsonschema import validate
 from jsonschema.exceptions import ValidationError
 
