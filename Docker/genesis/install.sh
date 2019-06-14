c695bf7e368b192e5dc8e1cf5829290ae578beb2
Jaechang Namgoong <jnamgoong@icon.foundation>
2019-06-14 18:01:54 +0900

    Add USE_PYPI option to install packages from PyPI

--- Docker/genesis/install.sh
+++ Docker/genesis/install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-WORKDIR=$(dirname $0)
-FILES=$(echo *.whl)
-for f in $FILES; do
-    pip3 install $WORKDIR/$f
-done
+USE_PYPI=true
+
+if [ $USE_PYPI == true ]; then
+    pip3 install tbears
+else
+    WORKDIR=$(dirname $0)
+    FILES=$(echo *.whl)
+    for f in $FILES; do
+        pip3 install $WORKDIR/$f
+    done
+fi
